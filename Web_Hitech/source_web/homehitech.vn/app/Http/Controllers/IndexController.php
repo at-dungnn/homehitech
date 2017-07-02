291acbc08153455d4cf2d2108748f2b09bdb1038
Hieu-PC\Hieu <nndungcse@gmail.com>
2017-07-02 10:19:29 +0700

    no message

--- Web_Hitech/source_web/homehitech.vn/app/Http/Controllers/IndexController.php
+++ Web_Hitech/source_web/homehitech.vn/app/Http/Controllers/IndexController.php
@@ -3,19 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Category;
-use App\Product;
-use App\Contact;
 class IndexController extends Controller
 {
     public function getIndex(){   
-    	$productLight1 		= Product::where('category_id','1')->get();
-    	$productLight2 		= Product::where('category_id','2')->get();
-    	$category 			= Category::where('delete','0')->get();
-    	$contact 			= Contact::first();
-    	return view('index',[	'productLight1'=> $productLight1,
-					    		'productLight2'=> $productLight2,
-					    		'category'     => $category,
-					    		'contact'      => $contact]);
+    	return view('layouts.master');
     }
 }
