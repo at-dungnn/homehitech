73805b227bced588701373e5f3882dddce7e1eb0
Ngovanviet90 <ngovanviet90@gmail.com>
2017-07-04 16:44:44 +0700

    2131

--- Web_Hitech/source_web/homehitech.vn/app/Http/Controllers/IndexController.php
+++ Web_Hitech/source_web/homehitech.vn/app/Http/Controllers/IndexController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Category;
 use App\Product;
+use App\Banner;
 class IndexController extends Controller
 {
     public function getIndex(){  
     	$category			=	Category::all();
         $menu               =   Category::where('parent_id',0)->get();
-    	$product 			= 	Product::where('delete',0)->take(20)->orderBy('id','desc')->get();
-    	// dd($category,$menu);
-    	return view('layouts.main-content',compact('category','menu','product'));
+        $product            =   Product::where('delete',0)->take(20)->orderBy('id','desc')->get();
+    	$banner 			= 	Banner::all();
+
+    	return view('layouts.main-content',compact('category','menu','product','banner'));
     }
 
     public function postSearch(Request $request){
